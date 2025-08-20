c63db6e5a64425d60f5fbc7da1ed2218d1b67b69
VictorPotenciano <vpotenv98@gmail.com>
2025-08-20 18:25:44 +0200

    ajustes del authOptions

--- src/app/api/user/changePassword/route.ts
+++ src/app/api/user/changePassword/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
 import bcrypt from "bcryptjs";
+import { authOptions } from "@/lib/auth";
 
 export async function PUT(request: Request) {
   try {
